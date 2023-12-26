4547e2906c98577cfe08e20ad68a38433b01a7c3
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:21:54 +0100

    Student.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/Student.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/Student.java
@@ -2,17 +2,30 @@ package com.auca.StudentRegistration.Model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Represents a student with a registration number, first name, date of birth,
+ * and a list of registrations associated with the student.
+ */
 @Entity
-@Table(name= "student")
+@Table(name = "student")
 public class Student {
+
     @Id
     @Column(name = "registration_number")
+    @NotBlank(message = "Registration number cannot be blank")
     private String registrationNumber;
+
+    @NotBlank(message = "First name cannot be blank")
     private String firstName;
-    private String dateOfBirth;
+
+    @NotNull(message = "Date of birth cannot be null")
+    private LocalDate dateOfBirth;
 
     @OneToMany(mappedBy = "student")
     @JsonBackReference
@@ -21,7 +34,7 @@ public class Student {
     public Student() {
     }
 
-    public Student(String registrationNumber, String firstName, String dateOfBirth, List<StudentRegistration> registrations) {
+    public Student(String registrationNumber, String firstName, LocalDate dateOfBirth, List<StudentRegistration> registrations) {
         this.registrationNumber = registrationNumber;
         this.firstName = firstName;
         this.dateOfBirth = dateOfBirth;
@@ -44,11 +57,11 @@ public class Student {
         this.firstName = firstName;
     }
 
-    public String getDateOfBirth() {
+    public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
@@ -58,5 +71,31 @@ public class Student {
 
     public void setRegistrations(List<StudentRegistration> registrations) {
         this.registrations = registrations;
+    }
+
+    // Optional: Implement equals and hashCode for object comparison
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(registrationNumber, student.registrationNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(registrationNumber);
+    }
+
+    // Optional: Implement toString for human-readable representation
+
+    @Override
+    public String toString() {
+        return "Student{" +
+               "registrationNumber='" + registrationNumber + '\'' +
+               ", firstName='" + firstName + '\'' +
+               ", dateOfBirth=" + dateOfBirth +
+               '}';
     }
 }
