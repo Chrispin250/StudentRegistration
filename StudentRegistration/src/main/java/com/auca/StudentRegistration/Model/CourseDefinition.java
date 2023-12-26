c5d1ab1bada7029600804ad6a4db22755d8410dd
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:17:00 +0100

    CourseDefinition.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/CourseDefinition.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/CourseDefinition.java
@@ -2,12 +2,24 @@ package com.auca.StudentRegistration.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
+/**
+ * Represents the definition of a course.
+ * This class is used to model course definitions, including code, name, and description.
+ */
 @Entity
 public class CourseDefinition {
+
     @Id
+    @NotBlank(message = "Code cannot be blank")
     private String code;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Description cannot be blank")
     private String description;
 
     public CourseDefinition() {
@@ -41,5 +53,31 @@ public class CourseDefinition {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    // Optional: Implement equals and hashCode for object comparison
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CourseDefinition that = (CourseDefinition) o;
+        return Objects.equals(code, that.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
+
+    // Optional: Implement toString for human-readable representation
+
+    @Override
+    public String toString() {
+        return "CourseDefinition{" +
+               "code='" + code + '\'' +
+               ", name='" + name + '\'' +
+               ", description='" + description + '\'' +
+               '}';
     }
 }
