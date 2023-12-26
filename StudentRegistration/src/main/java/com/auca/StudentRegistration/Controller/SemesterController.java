556e061956dccf5a79ccfb20a4a9cfcf075b0a89
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:02:05 +0100

    SemesterController.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Controller/SemesterController.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Controller/SemesterController.java
@@ -9,58 +9,60 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
-@RequestMapping(value = "/semester" , produces = (MediaType.APPLICATION_JSON_VALUE), consumes = (MediaType.APPLICATION_JSON_VALUE))
+@RequestMapping(value = "/semester", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class SemesterController {
-    private static final long serialVersionUID = 1L;
+
     @Autowired
     private SemesterService semService;
-    //creating
-    @PostMapping(value = "/saveSemester")
-    public ResponseEntity<?> createSemester(@RequestBody Semester semester){
-        if(semester != null ){
-            String message = semService.saveSemester(semester);
-            if(message != null){
-                return new ResponseEntity<>("Semester Saved Successfully", HttpStatus.CREATED);
-            }
-            else {
-                return new ResponseEntity<>("Semester Not Saved",HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        else {
-            return new ResponseEntity<>("Something went wrong",HttpStatus.BAD_GATEWAY);
-        }
-    }
 
-    //list
-    @GetMapping(value = "/listSemesters")
-    public ResponseEntity<List<Semester>> listSemester() {
-        List<Semester> sems = semService.listSemesters();
-        return new ResponseEntity<>(sems, HttpStatus.OK);
-    }
-    //update
-    @PutMapping(value = "/updateSemester/{code}")
-    public ResponseEntity<String> updateSemester(@PathVariable String code, @RequestBody Semester semester) {
+    @PostMapping(value = "/saveSemester")
+    public ResponseEntity<String> createSemester(@RequestBody Semester semester) {
         if (semester != null) {
-            String message = semService.updateSemester(code, semester);
-            if (message != null) {
-                return new ResponseEntity<>("Semester Updated Successfully", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Semester Not Updated Successfully", HttpStatus.NOT_FOUND);
+            try {
+                String message = semService.saveSemester(semester);
+                return new ResponseEntity<>("Semester Saved Successfully", HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Semester Not Saved: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
         }
     }
-    //delete
+
+    @GetMapping(value = "/listSemesters")
+    public ResponseEntity<List<Semester>> listSemesters() {
+        try {
+            List<Semester> semesters = semService.listSemesters();
+            return new ResponseEntity<>(semesters, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(value = "/updateSemester/{code}")
+    public ResponseEntity<String> updateSemester(@PathVariable String code, @RequestBody Semester semester) {
+        if (semester != null) {
+            try {
+                String message = semService.updateSemester(code, semester);
+                return new ResponseEntity<>("Semester Updated Successfully", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Semester Not Updated: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+        }
+    }
+
     @DeleteMapping(value = "/deleteSemester/{code}")
     public ResponseEntity<String> deleteSemester(@PathVariable String code) {
         if (code != null) {
-            String message = semService.deleteSemester(code);
-            if (message != null) {
+            try {
+                String message = semService.deleteSemester(code);
                 return new ResponseEntity<>("Semester Deleted Successfully", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Semester Not Deleted Successfully", HttpStatus.NOT_FOUND);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Semester Not Deleted: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
