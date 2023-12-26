12b99465fdcbbf9268ab628fae3285df874c8fc7
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:11:07 +0100

    TeacherController.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Controller/TeacherController.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Controller/TeacherController.java
@@ -1,9 +1,7 @@
 package com.auca.StudentRegistration.Controller;
 
-import com.auca.StudentRegistration.Model.Student;
-import com.auca.StudentRegistration.Model.Teacher;
-import com.auca.StudentRegistration.Service.StudentService;
-import com.auca.StudentRegistration.Service.TeacherService;
+import com.auca.StudentRegistration.Model.*;
+import com.auca.StudentRegistration.Service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,61 +9,109 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
-@RequestMapping(value = "/teacher" , produces = (MediaType.APPLICATION_JSON_VALUE), consumes = (MediaType.APPLICATION_JSON_VALUE))
-public class TeacherController {
-    private static final long serialVersionUID = 1L;
+@RequestMapping(value = "/StudCourse", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+public class StudentCrsController {
+
     @Autowired
-    private TeacherService teacherService;
-    //creating
-    @PostMapping(value = "/saveTeacher")
-    public ResponseEntity<?> createTeacher(@RequestBody Teacher teacher){
-        if(teacher != null ){
-            String message = teacherService.saveTeacher(teacher);
-            if(message != null){
-                return new ResponseEntity<>("Teacher Saved Successfully", HttpStatus.CREATED);
+    private StudCourseService studCrsService;
+
+    @Autowired
+    private SemesterService semesterService;
+
+    @Autowired
+    private StudRegistrationService regService;
+
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private CourseDefService defService;
+
+    @PostMapping(value = "/saveStudCourse")
+    public ResponseEntity<String> createStudCrs(@RequestBody StudentCourse studCrs) {
+        if (studCrs != null) {
+            try {
+                String message = studCrsService.saveStudCourse(studCrs);
+                return new ResponseEntity<>("Student Course Saved Successfully", HttpStatus.CREATED);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            else {
-                return new ResponseEntity<>("Teacher Not Saved",HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        else {
-            return new ResponseEntity<>("Something went wrong",HttpStatus.BAD_GATEWAY);
+        } else {
+            return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
         }
     }
 
-    //list
-    @GetMapping(value = "/listTeachers")
-    public ResponseEntity<List<Teacher>> listTeachers() {
-        List<Teacher> teachers = teacherService.listTeacher();
-        return new ResponseEntity<>(teachers, HttpStatus.OK);
+    @GetMapping(value = "/listStudCourse")
+    public ResponseEntity<List<StudentCourse>> listStudCrs() {
+        try {
+            List<StudentCourse> studCrs = studCrsService.listStudentsCourse();
+            return new ResponseEntity<>(studCrs, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-    //update
-    @PutMapping(value = "/updateTeacher/{tr_code}")
-    public ResponseEntity<String> updateTeacher(@PathVariable String tr_code, @RequestBody Teacher teacher) {
-        if (teacher != null) {
-            String message = teacherService.updateTeacher(tr_code, teacher);
-            if (message != null) {
-                return new ResponseEntity<>("Teacher Updated Successfully", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Teacher Not Updated Successfully", HttpStatus.NOT_FOUND);
+
+    @PutMapping(value = "/updateStudCourse/{id}")
+    public ResponseEntity<String> updateStudCourse(@PathVariable Integer id, @RequestBody StudentCourse studCrs) {
+        if (studCrs != null) {
+            try {
+                String message = studCrsService.updateStudCourse(id, studCrs);
+                return new ResponseEntity<>("Student Course Updated Successfully", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
         }
     }
-    //delete
-    @DeleteMapping(value = "/deleteTeacher/{tr_code}")
-    public ResponseEntity<String> deleteTeacher(@PathVariable String tr_code) {
-        if (tr_code != null) {
-            String message = teacherService.deleteTeacher(tr_code);
-            if (message != null) {
-                return new ResponseEntity<>("Teacher Deleted Successfully", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Teacher Not Deleted Successfully", HttpStatus.NOT_FOUND);
+
+    @DeleteMapping(value = "/deleteStudCourse/{id}")
+    public ResponseEntity<String> deleteStudCrs(@PathVariable Integer id) {
+        if (id != null) {
+            try {
+                String message = studCrsService.deleteStudCourse(id);
+                return new ResponseEntity<>("Student Course Deleted Successfully", HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } else {
             return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/listByCourse/{studentId}")
+    public ResponseEntity<List<StudentCourse>> listByCourse(@PathVariable String studentId) {
+        try {
+            StudentRegistration stud = regService.getRegistrationByStudentId(studentId);
+
+            if (stud != null) {
+                List<StudentCourse> crs = studCrsService.getCoursesByStudentId(studentId);
+                return new ResponseEntity<>(crs, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/listByCourseAndSemester/{courseCode}/{semesterId}")
+    public ResponseEntity<List<StudentCourse>> listByCourseAndSemester(
+            @PathVariable Integer courseCode,
+            @PathVariable String semesterId) {
+        try {
+            Course course = courseService.getCourseById(courseCode);
+
+            if (course != null) {
+                List<StudentCourse> crs = studCrsService.getStudentByCourseAndSemester(course, semesterId);
+                return new ResponseEntity<>(crs, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
