81e4a51ec28b2b4493fb4d3c63885cf67e03aefb
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:28:50 +0100

    CourseDefRepo.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Repository/CourseDefRepo.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Repository/CourseDefRepo.java
@@ -1,9 +1,11 @@
 package com.auca.StudentRegistration.Repository;
 
 import com.auca.StudentRegistration.Model.CourseDefinition;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseDefRepo extends JpaRepository<CourseDefinition,String> {
+public interface CourseDefRepo extends JpaRepository<CourseDefinition, String> {
 }
