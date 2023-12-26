289ce343f1843c5e8a2ddd7097ad08f0a6318e0f
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:14:07 +0100

    AcademicUnit.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/AcademicUnit.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/AcademicUnit.java
@@ -1,13 +1,21 @@
 package com.auca.StudentRegistration.Model;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
+/**
+ * Represents an academic unit.
+ * This class is used to model academic units, such as departments or faculties.
+ */
 @Entity
 public class AcademicUnit {
+
     @Id
+    @NotBlank(message = "Code cannot be blank")
     private String code;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
 
     @Enumerated(EnumType.STRING)
@@ -31,31 +39,42 @@ public class AcademicUnit {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public EAcademicUnit getUnitType() {
         return unitType;
     }
 
-    public void setUnitType(EAcademicUnit unitType) {
-        this.unitType = unitType;
-    }
-
     public AcademicUnit getParentUnit() {
         return parentUnit;
     }
 
-    public void setParentUnit(AcademicUnit parentUnit) {
-        this.parentUnit = parentUnit;
+    // Optional: Implement equals and hashCode for object comparison
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AcademicUnit that = (AcademicUnit) o;
+        return Objects.equals(code, that.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
+
+    // Optional: Implement toString for human-readable representation
+
+    @Override
+    public String toString() {
+        return "AcademicUnit{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", unitType=" + unitType +
+                ", parentUnit=" + parentUnit +
+                '}';
     }
 }
