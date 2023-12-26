6b9f459842bbf88ce1221fade4df465ec69ee293
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 14:20:46 +0100

    Semester.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/Semester.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/Model/Semester.java
@@ -2,15 +2,29 @@ package com.auca.StudentRegistration.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.Objects;
 
+/**
+ * Represents a semester with an id, name, start date, and end date.
+ * This class is used to model information about academic semesters.
+ */
 @Entity
 public class Semester {
+
     @Id
+    @NotBlank(message = "ID cannot be blank")
     private String id;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotNull(message = "Start date cannot be null")
     private LocalDate startDate;
+
+    @NotNull(message = "End date cannot be null")
     private LocalDate endDate;
 
     public Semester() {
@@ -53,5 +67,32 @@ public class Semester {
 
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
+    }
+
+    // Optional: Implement equals and hashCode for object comparison
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Semester semester = (Semester) o;
+        return Objects.equals(id, semester.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    // Optional: Implement toString for human-readable representation
+
+    @Override
+    public String toString() {
+        return "Semester{" +
+               "id='" + id + '\'' +
+               ", name='" + name + '\'' +
+               ", startDate=" + startDate +
+               ", endDate=" + endDate +
+               '}';
     }
 }
