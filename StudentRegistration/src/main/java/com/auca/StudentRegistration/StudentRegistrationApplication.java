7cdd5d61707f5bab3f7f14580fb9e66d48025323
Mugisha Ufitinema Chrispin <130361618+Chrispin250@users.noreply.github.com>
2023-12-26 15:01:36 +0100

    StudentRegistrationApplication.java

--- StudentRegistration/src/main/java/com/auca/StudentRegistration/StudentRegistrationApplication.java
+++ StudentRegistration/src/main/java/com/auca/StudentRegistration/StudentRegistrationApplication.java
@@ -3,11 +3,13 @@ package com.auca.StudentRegistration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the Student Registration Application.
+ */
 @SpringBootApplication
 public class StudentRegistrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentRegistrationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRegistrationApplication.class, args);
+    }
 }
